034179fe9b2102182a5b4c3d71eb3f63945cd1c4
Muzammil98 <muzammilsarwar0@gmail.com>
2022-03-18 01:00:22 +0500

    Added user list

--- AdminPanel/template/assets/js/custom-api.js
+++ AdminPanel/template/assets/js/custom-api.js
@@ -38,8 +38,23 @@ $(".admin-email").text(user.email);
 const getTransactionsData = () => {};
 
 const deleteUser = (id) => {
-  console.log("DEDEDE", id);
+  const token = localStorage.getItem("token");
+  $.ajax({
+    url: `${BASE_URL}/user/${id}`,
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  }).then((response) => {
+    window.location.replace("users.html");
+  });
 };
+
+$("#deleteUserBtn").click(() => {
+  const id = window.location.search.split("=")[1];
+  deleteUser(id);
+});
+
 const getUsersList = (e) => {
   e?.preventDefault();
 
@@ -65,24 +80,103 @@ const getUsersList = (e) => {
         }</td>
         <td>${item.createdAt}</td>
         <td>
-          <a href="update_user.html">
+          <a href="update_user.html?id=${item.id}">
           Edit
             <i data-feather="edit"></i>
           </a>
         </td>
-        <td>
-          <a class="delete-user" onclick="deleteUser(${item.id})" href="#">
-          Delete
-            <i data-feather="delete"></i>
-          </a>
-        </td>
+       
       </tr>`
     );
-
+    //   <td>
+    //   <a class="delete-user" onclick="deleteUser(${item.id})" href="#">
+    //   Delete
+    //     <i data-feather="delete"></i>
+    //   </a>
+    // </td>
     $("#dataTableExample tbody").html(htmlData);
   });
 };
 
 if (window.location.pathname.includes("users.html")) getUsersList();
 
+const getUserDetails = () => {
+  const id = window.location.search.split("=")[1];
+  const token = localStorage.getItem("token");
+
+  $.ajax({
+    url: `${BASE_URL}/user/${id}`,
+    headers: { Authorization: `Bearer ${token}` },
+  }).then((response) => {
+    $("#exampleInputFirstname1").val(response.firstname);
+    $("#exampleInputLastname1").val(response.lastname);
+    $("#exampleInputEmail1").val(response.email);
+  });
+
+  $.ajax({
+    url: `${BASE_URL}/wallet/user/${id}`,
+    headers: { Authorization: `Bearer ${token}` },
+  }).then((response) => {
+    const walletId = response.id || response._id;
+    localStorage.setItem("walletId", walletId);
+    $("#exampleInputBuyingPower1").val(response.buying);
+  });
+};
+
+if (window.location.pathname.includes("update_user.html")) getUserDetails();
+
 $(".delete-user").click(() => deleteUser());
+
+const updateUserFunction = (e) => {
+  e?.preventDefault();
+  const id = window.location.search.split("=")[1];
+  const token = localStorage.getItem("token");
+
+  const firstname = $("#exampleInputFirstname1").val();
+  const lastname = $("#exampleInputLastname1").val();
+
+  const payload = {
+    firstname,
+    lastname,
+  };
+
+  $.ajax({
+    url: `${BASE_URL}/user/${id}`,
+    method: "PUT",
+    data: payload,
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  }).then((response) => {
+    console.log("UPDATE WALLET");
+    window.location.reload();
+  });
+};
+
+$("#updateUserBtn").click(() => updateUserFunction());
+
+const updateUserWallet = (e) => {
+  e?.preventDefault();
+
+  const walletId = localStorage.getItem("walletId");
+  const token = localStorage.getItem("token");
+
+  const buying = $("#exampleInputBuyingPower1").val();
+  // equity, profit, loss
+  const payload = { buying };
+
+  console.log("PAYLOAD", payload);
+  $.ajax({
+    url: `${BASE_URL}/wallet/${walletId}`,
+    method: "PUT",
+    data: payload,
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  }).then((response) => {
+    console.log("UPDATE WALLET", response);
+    window.location.reload();
+  });
+};
+
+$("#updateUserWalletBtn").click(() => updateUserWallet());
